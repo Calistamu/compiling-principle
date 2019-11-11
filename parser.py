1383763a356e8201a1feafa01ed5f162c4b33a02
mudou <41284875+Calistamu@users.noreply.github.com>
2019-11-11 18:38:15 +0800

    增加LL1判断及栈变化过程

--- parser.py
+++ parser.py
@@ -1,105 +1,123 @@
-from collections import defaultdict
+#!/usr/env python
+# -*- coding: UTF-8 -*-
+"""
+ 文法：
+    E->E+T | T
+    T->T*F | F
+    F->(E)|i
+ 消除左递归：
+    E->TH
+    H->+TH|e(一普赛肋)
+    T->FY
+    Y->*FY|e
+    F->(E)|i
+ 非终结符：
+    E，H，T，Y，F
+ 终结符:
+    i,+,*,(,),#
+"""
+# 手动构造预测分析表
+dists = {
+    ('E', 'i'): 'TH',
+    ('E', '('): 'TH',
+    ('H', '+'): '+TH',
+    ('H', ')'): 'e',
+    ('H', '#'): 'e',
+    ('T', 'i'): 'FY',
+    ('T', '('): 'FY',
+    ('Y', '+'): 'e',
+    ('Y', '*'): '*FY',
+    ('Y', ')'): 'e',
+    ('Y', '#'): 'e',
+    ('F', 'i'): 'i',
+    ('F', '('): '(E)',
+}
 
-def addtwodimdict(thedict, key_a, key_b, val):     
-    if key_a in thedict:        
-        thedict[key_a].update({key_b: val})    
-    else:        
-        thedict.update({key_a:{key_b: val}})  
+# 构造终结符集合
+Vt = ('i', '+', '*', '(', ')')
 
-def GetFirst(stack_item):    
-    if stack_item[1] in vt:#产生式第一个为终结        
-        first[stack_item[0]].add(stack_item[1])#将其放入第一个的first    
-    else:        
-        for find_item in stack:            
-            if find_item[0]==stack_item[1]:                
-                GetFirst(find_item)                
-                first[stack_item[0]]=first[stack_item[1]]|first[stack_item[0]] 
+# 构造非终结符集合
+Vh = ('E', 'H', 'T', 'Y', 'F')
 
-def GetFollow(vi_item):    
-    for i in stack:        
-        j=1        
-        while j<len(i)-1:            
-            if (i[j]==vi_item)&(i[j+1] in vt): # 存在 Ab 则把b放入                
-                follow[vi_item].add(i[j+1])            
-            if (i[j]==vi_item)&(i[j+1] in vi): #存在AB 则把B的first放入                
-                follow[vi_item]=follow[vi_item]|first[i[j+1]]-{'ε'}            
-            if (i[j] in vi)&('ε' in first[i[j+1]]): #存在 A=EBC 且C的first中有空                
-                follow[i[j]]=follow[i[0]]|follow[i[j]]            
-            j+=1        
-        if i[len(i)-1] ==vi_item:#为某行最后一个字符            
-            follow[vi_item].add('$')          
-        if (i[0]==vi_item)&(i[len(i)-1] in vi):#A=....B            
-            follow[i[len(i)-1]]=follow[vi_item]|follow[i[len(i)-1]]        
-        if (i[len(i)-1]==vi_item)&(i[0] in vi):            
-            follow[vi_item]=follow[i[0]]|follow[i[len(i)-1]]                 
 
-vt=['i','+','*','(',')','ε'] #终结字符
-vi=['E','e','T','t','F'] #非终结字符
-gramma=open('C:/Users/karen/Documents/GitHub/compiling-principle/test.txt').readlines()
-stack=[]
-for i in gramma:    
-    ss=i[0:1]    
-    j=0    
-    while j<len(i):        
-        if i[j]=='>':            
-            break        
-        j+=1    
-    j+=1 #找到->后的第一个位置    
-    while j<len(i):        
-        if i[j]=='\n':            
-            break        
-        if i[j]!='|':            
-            ss+=i[j]        
-        else:            
-            stack.append(ss)            
-            ss=i[0:1]        
-        j+=1    
-    stack.append(ss)
-first=defaultdict(set) #构建元素映射到多个元素（集合）的字典
-follow=defaultdict(set)
-for stack_item in stack:    
-    GetFirst(stack_item)    
+# 获取输入栈中的内容
+def printstack(stack):
+    rtu = ''
+    for i in stack:
+        rtu += i
+    return rtu
 
-follow['E'].add('$')   
-for vi_item in vi:    
-    GetFollow(vi_item)
-ana_table=dict() #建立预测分析表 其中key为非终结符和输入符号字符串的相连接 value为对应的分析结果  
 
-addtwodimdict(ana_table, 'E', 'i', 'E->Te')
-addtwodimdict(ana_table, 'E', '(', 'E->Te')
-addtwodimdict(ana_table, 'e', '+', 'e->+Te')
-addtwodimdict(ana_table, 'e', ')', 'e->ε')
-addtwodimdict(ana_table, 'e', '$', 'e->ε')
-addtwodimdict(ana_table, 'T', 'i', 'T->Ft')
-addtwodimdict(ana_table, 'T', '(', 'T->Ft')
-addtwodimdict(ana_table, 't', '+', 't->ε')
-addtwodimdict(ana_table, 't', '*', 't->*Ft')
-addtwodimdict(ana_table, 't', ')', 't->ε')
-addtwodimdict(ana_table, 't', '$', 't->ε')
-addtwodimdict(ana_table, 'F', 'i', 'F->i')
-addtwodimdict(ana_table, 'F', ')', 'F->(E)')     
+# 得到输入串剩余串
+def printstr(str, index):
+    rtu = ''
+    for i in range(index, len(str), 1):
+        rtu += str[i]
+    return rtu
 
-sen="i*i+i$"
-ip=0 
-ss=['$','E']
-while ss[len(ss)-1]!='$':    
-    print(ss)    
-    print(sen[ip])    
-    if ss[len(ss)-1]==sen[ip]:        
-        ss.pop()        
-        ip+=1    
-    elif ss[len(ss)-1] in vt:        
-        print("error1")        
-        break    
-    elif sen[ip] not in ana_table[ss[len(ss)-1]]:        
-        print("error2")        
-        break    
-    elif sen[ip] in ana_table[ss[len(ss)-1]]:        
-        strings=ana_table[ss[len(ss)-1]][sen[ip]]        
-        print(strings)        
-        ss.pop()        
-        j=len(strings)-1        
-        while j>2:            
-            if strings[j]!='ε':                
-                ss.append(strings[j])            
-            j-=1
+
+# 定义error函数
+def error():
+    print('Error')
+    exit()
+
+
+# 总控程序
+def masterctrl(str):
+    '''
+    总控程序，用于进程文法的判断
+    '''
+    # 用列表模拟栈
+    stack = []
+    location = 0
+    # 将#号入栈
+    stack.append(str[location])
+
+    # 将文法开始符入栈
+    stack.append('E')
+    # 将输入串第一个字符读进a中
+    location += 1
+    a = str[location]
+    printstack(stack)
+    flag = True
+    count = 0
+    print('%d\t\t%s\t\t%s' % (count, printstack(stack), printstr(str, location)))
+    while flag:
+        if count == 0:
+            pass
+        else:
+            if x in Vt:
+                print('%d\t\t%s\t\t%s' % (count, printstack(stack), printstr(str, location)))
+            else:
+                print('%d\t\t%s\t\t%s\t\t%s->%s' % (count, printstack(stack), printstr(str, location), x, s))
+        x = stack.pop()
+        if x in Vt:
+            if x == str[location]:
+                location += 1
+                a = str[location]
+            else:
+                error()
+        elif x == '#':
+            if x == a:
+                flag = False
+            else:
+                error()
+        elif (x, a) in dists.keys():
+            s = dists[(x, a)]
+            for i in range(len(s) - 1, -1, -1):
+                if s[i] != 'e':
+                    stack.append(s[i])
+        else:
+            error()
+        count += 1
+
+
+def main():
+    # str = input()
+    str = '#i*i+i#'
+    print("步骤\t\t符号栈\t\t输入串\t\t\t所用产生式")
+    masterctrl(str)
+
+
+if __name__ == '__main__':
+    main()
